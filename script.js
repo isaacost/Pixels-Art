0afdc8b4b40fb1a4d3709887575c0f5ebc376733
Isa Costa <isa-hcosta@hotmail.com>
2022-07-04 20:18:21 -0300

    tentar arrumar erros no lint

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const pixelBoard = document.getElementById('pixel-board');
 function linhas(n) {
-    for (let i = 1; i <= n; i += 1) {
-        const criaDiv = document.createElement('div');
-        criaDiv.className = 'pixel';
-        pixelBoard.appendChild(criaDiv);
-    }
+  for (let i = 1; i <= n; i += 1) {
+    const criaDiv = document.createElement('div');
+    criaDiv.className = 'pixel';
+    pixelBoard.appendChild(criaDiv);
+  }
 }
 
 function criaPixels(n) {
-    for (let i = 1; i <= n; i += 1) {
-        linhas(n);
-    }
+  for (let i = 1; i <= n; i += 1) {
+    linhas(n);
+  }
 }
 criaPixels(5);
 
@@ -20,9 +20,9 @@ const corTres = document.querySelector('.cor3');
 const corQuatro = document.querySelector('.cor4');
 
 function adicionandoSelected(event) {
-    const classeSelected = document.querySelector('.selected');
-    classeSelected.classList.remove('selected');
-    event.target.classList.add('selected');
+  const classeSelected = document.querySelector('.selected');
+  classeSelected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
 corUm.addEventListener('click', adicionandoSelected);
@@ -33,24 +33,24 @@ corQuatro.addEventListener('click', adicionandoSelected);
 let corSelecionada = 'cor1';
 const pegaPixel = document.querySelectorAll('.pixel');
 function mudaCor() {
-    corUm.addEventListener('click', function () {
-        corSelecionada = 'cor1';
-    });
-    corDois.addEventListener('click', function () {
-        corSelecionada = 'cor2';
-    });
-    corTres.addEventListener('click', function () {
-        corSelecionada = 'cor3';
-    });
-    corQuatro.addEventListener('click', function () {
-        corSelecionada = 'cor4';
-    });
+  corUm.addEventListener('click', function () {
+    corSelecionada = 'cor1';
+  });
+  corDois.addEventListener('click', function () {
+    corSelecionada = 'cor2';
+  });
+  corTres.addEventListener('click', function () {
+    corSelecionada = 'cor3';
+  });
+  corQuatro.addEventListener('click', function () {
+    corSelecionada = 'cor4';
+  });
 
-    for (let i = 0; i < pegaPixel.length; i +=1){
+  for (let i = 0; i < pegaPixel.length; i += 1) {
     pegaPixel[i].addEventListener('click', function () {
-        pegaPixel[i].className = 'pixel ' + corSelecionada;
+      pegaPixel[i].className = 'pixel ' + corSelecionada;
     });
-}
+  }
 }
 
 mudaCor();
